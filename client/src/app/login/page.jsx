2697984947b55afc7be921f0bb5fa3e9f2fc7b86
JavierS084 <javier.sanchezb084@gmail.com>
2023-07-25 19:03:00 -0400

    Ft login

--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -1,51 +1,79 @@
-import axios from "axios";
-import { useState } from "react";
-import { useRouter } from "next/router";
+"use client"
+import React, { useState, useSelector, useDispatch, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { LoginUser, reset, getMe } from "../../auth/authSlice";
+export const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-function Home() {
-  const [credentials, setCredentials] = useState({
-    email: "",
-    password: "",
-  });
+  const [shown, setShown] = useState(false);
+  const dispatch = useDispatch();
   const router = useRouter();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const res = await axios.post("/api/auth/login", credentials);
-    console.log(res);
-
-    if (res.status === 200) {
-      router.push("/dashboard");
-    }
+  const switchShown = () => {
+    setShown(!shown);
   };
 
+  const { user, isError, isSuccess, isLoading, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (user || isSuccess) {
+      router.push("/");
+    }
+    dispatch(getMe());
+    dispatch(reset());
+  }, [user, isSuccess, dispatch, router]);
+
+  const Auth = (e) => {
+    e.preventDefault();
+    dispatch(LoginUser({ email, password }));
+  };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="email"
-          onChange={(e) =>
-            setCredentials({
-              ...credentials,
-              email: e.target.value,
-            })
-          }
-        />
-        <input
-          type="password"
-          placeholder="password"
-          onChange={(e) =>
-            setCredentials({
-              ...credentials,
-              password: e.target.value,
-            })
-          }
-        />
-        <button>Save</button>
+    <div className="row">
+      <form onSubmit={Auth}>
+        <div className="login col-md-3 mx-auto ">
+          <h3 className="Auth-form-title">Inicio Sesi&oacute;n</h3>
+
+          <div className="form-group flex-column d-flex">
+            {isError && <p className="error">{message}</p>}
+            <label className="label">Email</label>
+
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
+          </div>
+          <div className="form-group flex-column d-flex">
+            <label className="label">Password</label>
+            <input
+              type={shown ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="*******"
+            />
+
+              {shown ? (
+                <i className="col align-self-end fas fa-eye" onClick={switchShown} />
+              ) : (
+                <i className="col align-self-end fas fa-eye-slash fas-eye" onClick={switchShown} />
+              )
+              }
+            
+            
+          </div>
+          <div className="form-group flex-column d-flex">
+            <button type="submit" className="btn btn-primary">
+              {isLoading ? "Cargando..." : "Iniciar Sesión"}
+            </button>
+          </div>
+        </div>
       </form>
     </div>
   );
-}
+};
 
-export default Home;
+export default LoginPage;
